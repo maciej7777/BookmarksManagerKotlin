21f5ad19fef9b84e4c6d84a8baa49f9eaf1158ee
Maciej Uniejewski <maciej.uniejewski@gmail.com>
2020-10-31 11:17:38 +0100

    Bump kotlin version

--- src/main/kotlin/com/uniejewskim/bookmarks/controller/BookmarksController.kt
+++ src/main/kotlin/com/uniejewskim/bookmarks/controller/BookmarksController.kt
@@ -1,10 +1,12 @@
 package com.uniejewskim.bookmarks.controller
 
+import com.uniejewskim.bookmarks.model.Bookmark
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
+import java.util.Collections.emptyList
 
 @RestController()
 @RequestMapping("/bookmarks")
@@ -13,7 +15,10 @@ class BookmarksController {
     fun addBookmark() = "Controller"
 
     @GetMapping("/get")
-    fun getByUuid(@RequestParam(value = "id") id: UUID) = "Controller"
+    fun getByUuid(@RequestParam(value = "id") id: UUID) = run {
+        var b = Bookmark(UUID.randomUUID(), " ", " ", emptyList())
+        "Controller"
+    }
 
     @GetMapping("")
     fun showAllBookmarks() = "show"
